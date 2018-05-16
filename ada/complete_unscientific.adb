c397f4849e8b4cf12ddb63c299e752b33b0fd6a3
AdaDoom3 <squirek@adacore.com>
2018-05-16 08:49:37 -0400

    Update complete_unscientific.adb

--- ada/complete_unscientific.adb
+++ ada/complete_unscientific.adb
@@ -1,25 +1,24 @@
-with Ada.Integer_Text_IO;
-with Ada.Text_IO;
-
-with Tree; use Tree;
+with Ada.Text_IO; use Ada.Text_IO;
+with Trees;       use Trees;
 
 procedure Complete_Unscientific is
-  t: Tree.Tree;
-  cur: Integer := 5;
-  res: Integer := 0;
-  mode: Integer;
-  procedure PutI(item: Integer; width: Natural := 0; base: Natural := 10)
-    renames Ada.Integer_Text_IO.Put;
-  procedure New_Line(spacing: Ada.Text_IO.Positive_Count := 1) renames Ada.Text_IO.New_Line;
+  Tree   : Tree_Type;
+  Cursor : Integer := 5;
+  Result : Integer := 0;
+
 begin
-  initialize;
-  for i in 1..999999 loop
-    mode := i mod 3;
-    cur := (cur * 57 + 43) mod 10007;
-    if mode = 0 then insert(t, cur);
-    elsif mode = 1 then erase(t, cur);
-    else res := res + (if hasValue(t, cur) then 1 else 0);
-    end if;
+  for I in 1..999999 loop
+    Cursor := (Cursor * 57 + 43) mod 10007;
+
+    case I mod 3 is
+      when 0 => Insert (Tree, Cursor);
+      when 1 => Erase (Tree, Cursor);
+      when others => 
+        if Has_Value (Tree, Cursor) then
+          Result := Result + 1;
+        end if;
+    end case;
   end loop;
-  PutI(res); New_Line(1);
-end Complete_Unscientific;
+
+  Put_Line (Result'Image);
+end;
